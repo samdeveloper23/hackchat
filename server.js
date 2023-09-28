e587e877599a1a0c419814d9d8d21db6f51d5faa
samdeveloper23 <sam.developer.23@gmail.com>
2023-09-29 01:41:17 +0200

    tengo sueño y queir dormir

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const path = require("path");
+const PORT = process.env.PORT || 6677;
+const list_users = {};
+
+app.use(express.static(path.join(__dirname, "views")));
+
+server.listen(PORT, () => {
+  console.log(`Server listen: ${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  socket.on("register", (nickname) => {
+    if (list_users[nickname]) {
+      socket.emit("userExists");
+      return;
+    } else {
+      list_users[nickname] = socket.id;
+      socket.nickname = nickname;
+      socket.emit("login");
+      io.emit("activeSessions", list_users);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    delete list_users[socket.nickname];
+    io.emit("activeSessions", list_users);
+  });
+
+  socket.on("sendMessage", ({ message, image }) => {
+    io.emit("sendMessage", { message, user: socket.nickname, image });
+  });
+
+  socket.on("sendMessagesPrivate", ({ message, image, selectUser }) => {
+    if (list_users[selectUser]) {
+      io.to(list_users[selectUser]).emit("sendMessage", {
+        message,
+        user: socket.nickname,
+        image,
+      });
+      io.to(list_users[socket.nickname]).emit("sendMessage", {
+        message,
+        user: socket.nickname,
+        image,
+      });
+    } else {
+      alert("El usuario al que intentas enviar el mensaje no existe!");
+    }
+  });
+});
---
+++ views/js/client.js
@@ -0,0 +1,181 @@
+const socket = io.connect('http://eva00.sytes.net', { 'forceNew': true });
+
+let fileURL;
+
+//  Formularios
+const formLogin = document.querySelector("#formLogin");
+const formContentChat = document.querySelector(".body-chat");
+const formShowUsers = document.querySelector("#formShowUsers");
+const formChatGrupal = document.querySelector("#formChatGrupal");
+
+//  Textbox's
+const txtUserNickName = document.querySelector("#userNickName");
+const txtUserMessage = document.querySelector("#userMessage");
+
+//  File - Image
+const userFile = document.querySelector("#userFile");
+
+//  Button's
+const btnrRegisterUser = document.querySelector("#registerUser");
+const btnSendMessage = document.querySelector("#sendMessage");
+const btnSendFile = document.querySelector("#sendFile");
+
+//  Print
+const printUsersActive = document.querySelector("#usersActive");
+const printMessages = document.querySelector("#messages");
+
+formContentChat.style.display = "none";
+formShowUsers.style.display = "none";
+formChatGrupal.style.display = "none";
+
+socket.on("login", () => {
+  alert(
+    "¡Bienvenido " +
+    txtUserNickName.value.trim() +
+    "!\nRecuerda, respetar a los demás usuarios."
+  );
+  formLogin.style.display = "none";
+  formContentChat.style.display = "flex";
+  formShowUsers.style.display = "block";
+  formChatGrupal.style.display = "block";
+});
+
+socket.on("userExists", () => {
+  alert(
+    "El nickname: " +
+    txtUserNickName.value.trim() +
+    " ya está en uso, intenta con otro."
+  );
+  txtUserNickName.value = "";
+});
+
+socket.on("activeSessions", (users) => {
+  printUsersActive.innerHTML = "";
+  for (const user in users) {
+    printUsersActive.insertAdjacentHTML("beforeend", `<li>${user}</li>`);
+  }
+});
+
+socket.on("sendMessage", ({ message, user, image }) => {
+  printMessages.insertAdjacentHTML(
+    "beforeend",
+    `<div class="message frnd_message"><p>${message}<br /><span>${user}</span></p></div>`
+  );
+  if (image !== undefined) {
+    const imagen = document.createElement("img");
+    imagen.src = image;
+    printMessages.appendChild(imagen);
+  }
+  printMessages.scrollTop = printMessages.scrollHeight;
+});
+
+txtUserNickName.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    btnrRegisterUser.click();
+  }
+});
+
+btnrRegisterUser.addEventListener("click", () => {
+  if (txtUserNickName.value.trim() != "") {
+    let username = txtUserNickName.value.trim();
+    socket.emit("register", username);
+  }
+});
+
+btnSendMessage.addEventListener("click", () => {
+  if (fileURL != undefined) {
+    if (txtUserMessage.value.startsWith("-private:")) {
+      const selectUser = txtUserMessage.value.split(" ")[1];
+      const message = txtUserMessage.value.substr(selectUser.length + 10);
+      socket.emit("sendMessagesPrivate", {
+        message,
+        image: fileURL,
+        selectUser,
+      });
+    } else {
+      socket.emit("sendMessage", {
+        message: txtUserMessage.value.trim(),
+        image: fileURL,
+      });
+    }
+  } else {
+    if (txtUserMessage.value.trim() != "") {
+      if (txtUserMessage.value.startsWith("-private:")) {
+        const selectUser = txtUserMessage.value.split(" ")[1];
+        const message = txtUserMessage.value.substr(selectUser.length + 10);
+        socket.emit("sendMessagesPrivate", {
+          message,
+          image: fileURL,
+          selectUser,
+        });
+      } else {
+        socket.emit("sendMessage", {
+          message: txtUserMessage.value.trim(),
+          image: fileURL,
+        });
+      }
+    }
+  }
+
+  txtUserMessage.value = "";
+  fileURL = undefined;
+  printMessages.scrollTop = printMessages.scrollHeight;
+});
+
+txtUserMessage.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    if (fileURL != undefined) {
+      if (txtUserMessage.value.startsWith("-private:")) {
+        const selectUser = txtUserMessage.value.split(" ")[1];
+        const message = txtUserMessage.value.substr(selectUser.length + 10);
+        socket.emit("sendMessagesPrivate", {
+          message,
+          image: fileURL,
+          selectUser,
+        });
+      } else {
+        socket.emit("sendMessage", {
+          message: txtUserMessage.value.trim(),
+          image: fileURL,
+        });
+      }
+    } else {
+      if (txtUserMessage.value.trim() != "") {
+        if (txtUserMessage.value.startsWith("-private:")) {
+          const selectUser = txtUserMessage.value.split(" ")[1];
+          const message = txtUserMessage.value.substr(selectUser.length + 10);
+          socket.emit("sendMessagesPrivate", {
+            message,
+            image: fileURL,
+            selectUser,
+          });
+        } else {
+          socket.emit("sendMessage", {
+            message: txtUserMessage.value.trim(),
+            image: fileURL,
+          });
+        }
+      }
+    }
+    txtUserMessage.value = "";
+    fileURL = undefined;
+  }
+
+  printMessages.scrollTop = printMessages.scrollHeight;
+});
+
+btnSendFile.addEventListener("click", () => {
+  userFile.click();
+});
+
+userFile.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    fileURL = reader.result;
+  };
+  reader.readAsDataURL(file);
+  fileURL
+    ? alert("Error al adjuntar, seleccione nuevamente.")
+    : alert("Foto adjunta, lista para enviar.");
+});
